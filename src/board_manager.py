a64f039c13e9b335f5932f45ba87ec17d4717395
Sam Kelemen <86901068+samkelemen@users.noreply.github.com>
2025-09-11 12:52:20 -0500

    Create board_manager.py

---
+++ src/board_manager.py
@@ -0,0 +1,16 @@
+class BoardManager:
+  def __init__(grid_size: int, mine_count: int):
+    pass
+
+  def place_mines():
+    pass
+
+  def get_cell(row: int, column: int):
+    pass
+
+  def neighbors(row: int, column: int) -> List[Cell]:
+    pass
+
+  def countAjdacentMines(row: int, column: int) -> int:
+    pass
+  
