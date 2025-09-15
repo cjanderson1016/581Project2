e966b9bab87ec7f6c287dd59229f04e57aac2725
Genea Dinnall <g923d101@ku.edu>
2025-09-15 15:10:39 -0500

    updated UI

--- src/UI_renderer.py
+++ src/UI_renderer.py
@@ -1,13 +1,18 @@
 import tkinter as tk
 from tkinter import messagebox
+from cell import Cell
+from gameLogic import GameLogic
 
 class GameGUI:
     def __init__(self, board):
         self.board = board
         self.root = tk.Tk()
         self.root.title("Minesweeper")
+        self.mineCount = None
+        self.statusLabel = None
+        self.game = GameLogic
     
-    def buildGrid(self):
+    def renderBoard(self):
         for y in range(len(self.board)):
             for x in range(len(self.board)):
                 button = tk.Button(self.root, text=' ', width=3, command=lambda y=y, x=x: self.reveal(y,x))
@@ -17,6 +22,11 @@ class GameGUI:
     def run(self):
         self.root.mainloop()
 
-board = [[0,0,0],[0,0,0],[0,0,0]]
-gui=GameGUI(board)
-gui.run()
+    def renderCell(self, cell:Cell, row:int, col:int):
+        pass
+
+    def updateStatus(self, status:str):
+        pass
+
+    def updateMineCount(self, count:int):
+        pass
